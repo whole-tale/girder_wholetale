410f24e1300e3c427ddf654b3eb460cce80661b3
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2022-02-07 10:28:33 -0600

    Simplify workspace handling

--- server/tasks/import_tale.py
+++ server/tasks/import_tale.py
@@ -7,17 +7,14 @@ import os
 import pathlib
 import sys
 import traceback
-from webdavfs.webdavfs import WebDAVFS
 from fs.osfs import OSFS
 from fs.copy import copy_fs
 from girder import events
 from girder.api.rest import setCurrentUser
-from girder.constants import TokenScope
 from girder.models.folder import Folder
 from girder.models.user import User
-from girder.models.token import Token
-from girder.utility import config, parseTimestamp
-from girder.plugins.jobs.constants import JobStatus, REST_CREATE_JOB_TOKEN_SCOPE
+from girder.utility import parseTimestamp
+from girder.plugins.jobs.constants import JobStatus
 from girder.plugins.jobs.models.job import Job
 
 from ..constants import CATALOG_NAME, TaleStatus
@@ -35,9 +32,6 @@ def run(job):
     tale_dir, manifest_file = job["args"]
     user = User().load(job["userId"], force=True)
     tale = Tale().load(job["kwargs"]["taleId"], user=user)
-    token = Token().createToken(
-        user=user, days=0.5, scope=(TokenScope.USER_AUTH, REST_CREATE_JOB_TOKEN_SCOPE)
-    )
 
     progressTotal = 3
     progressCurrent = 0
@@ -87,7 +81,7 @@ def run(job):
                 eventName="tale.update_citation", info={"tale": tale, "user": user}
             )
 
-        # 4. Copy data to the workspace using WebDAVFS (if it exists)
+        # 4. Copy data to the workspace
         progressCurrent += 1
         jobModel.updateJob(
             job,
@@ -97,20 +91,13 @@ def run(job):
             progressMessage="Copying files to workspace",
         )
         orig_tale_id = pathlib.Path(manifest_file).parts[0]
-        for workdir in ("workspace", "data/workspace", None):
-            if workdir:
-                workdir = os.path.join(orig_tale_id, workdir)
-                if os.path.isdir(workdir):
-                    break
-
-        if workdir:
-            password = "token:{_id}".format(**token)
-            root = "/tales/{_id}".format(**tale)
-            url = "http://localhost:{}".format(config.getConfig()["server.socket_port"])
-            with WebDAVFS(
-                url, login=user["login"], password=password, root=root
-            ) as webdav_handle:
-                copy_fs(OSFS(workdir), webdav_handle)
+        for workdir in ("workspace", "data/workspace"):
+            workdir = os.path.join(orig_tale_id, workdir)
+            if os.path.isdir(workdir):
+                workspace = Folder().load(tale["workspaceId"], force=True)
+                workspace_path = pathlib.Path(workspace["fsPath"])
+                copy_fs(OSFS(workdir), OSFS(workspace_path))
+                break
 
         # Create a version
         version_obj = mp.manifest.get(
