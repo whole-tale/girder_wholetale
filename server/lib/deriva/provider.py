61427c22748e7eedb3e4b8c29e30fd4583ae5398
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2022-02-12 08:54:54 -0600

    [deriva] define getDatasetUID

--- server/lib/deriva/provider.py
+++ server/lib/deriva/provider.py
@@ -1,6 +1,8 @@
 import pathlib
 from urllib.parse import urlparse
 
+from girder.models.folder import Folder
+from girder.models.item import Item
 from girder.models.setting import Setting
 from ...constants import PluginSettings
 from ..data_map import DataMap
@@ -28,3 +30,13 @@ class DerivaProvider(BDBagProvider):
         if 'name' in entity:
             name = entity['name']
         return DataMap(entity.getValue(), size=sz, repository='DERIVA', name=name)
+
+    def getDatasetUID(self, doc: object, user: object) -> str:
+        if "folderId" in doc:
+            path_to_root = Item().parentsToRoot(doc, user=user)
+        else:
+            path_to_root = Folder().parentsToRoot(doc, user=user)
+        # Collection{WT Catalog} / Folder{WT Catalog} / Folder{Deriva ds root}
+        if len(path_to_root) == 2:
+            return doc["meta"]["identifier"]
+        return path_to_root[2]["object"]["meta"]["identifier"]
