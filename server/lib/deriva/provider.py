cc18930e1cbc1c9dc04e7d1c1ca381deee99803e
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2022-02-14 11:42:23 -0600

    [deriva] record identifier that's available via minid

--- server/lib/deriva/provider.py
+++ server/lib/deriva/provider.py
@@ -12,7 +12,7 @@ from ..bdbag.bdbag_provider import BDBagProvider
 
 class DerivaProvider(BDBagProvider):
     def __init__(self) -> None:
-        super().__init__('DERIVA')
+        super().__init__("DERIVA")
 
     def matches(self, entity: Entity) -> bool:
         deriva_urls = Setting().get(PluginSettings.DERIVA_EXPORT_URLS)
@@ -23,13 +23,16 @@ class DerivaProvider(BDBagProvider):
         return False
 
     def lookup(self, entity: Entity) -> DataMap:
-        sz = -1
-        if 'size' in entity:
-            sz = entity['size']
-        name = pathlib.Path(urlparse(entity.getValue()).path).with_suffix('').name
-        if 'name' in entity:
-            name = entity['name']
-        return DataMap(entity.getValue(), size=sz, repository='DERIVA', name=name)
+        url = entity.getValue()
+        default_name = pathlib.Path(urlparse(url).path).with_suffix("").name
+        return DataMap(
+            url,
+            entity.dict.get("size", -1),
+            doi=entity.dict.get("identifier"),
+            name=entity.dict.get("name", default_name),
+            repository="DERIVA",
+            tale=False,
+        )
 
     def getDatasetUID(self, doc: object, user: object) -> str:
         if "folderId" in doc:
