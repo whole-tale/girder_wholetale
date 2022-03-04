7dc5c4a0972dfc2336b562205e536d614eeec67a
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2022-03-04 12:38:38 -0600

    Handle importing Tales from DataONE

--- server/lib/dataone/__init__.py
+++ server/lib/dataone/__init__.py
@@ -16,6 +16,24 @@ class DataONELocations:
     dev_cn = "https://cn-stage.test.dataone.org/cn/v2"
 
 
+class DataONENotATaleError(Exception):
+    """Exception raised if DataONE record is not a Tale.
+
+    Attributes:
+        pid - identifier of the record.
+        base_url - CN url used to query the record.
+
+    """
+
+    def __init__(
+        self, pid, base_url, message="DataONE package ({}) is not a Tale (CN: {})"
+    ):
+        self.pid = pid
+        self.base_url = base_url
+        self.message = message.format(self.pid, self.base_url)
+        super().__init__(self.message)
+
+
 addProvider(type("DataONEDev", (FakeDataONE,), {}))
 addProvider(type("DataONEProd", (FakeDataONE,), {}))
 addProvider(type("DataONEStage", (FakeDataONE,), {}))
